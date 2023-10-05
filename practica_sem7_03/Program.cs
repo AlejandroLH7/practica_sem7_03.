242c4884fd14a59fe9c9b6f723d5b1eec93bbc3c
audio <audio@LAPTOP-A2DKNN9M>
2023-10-04 22:36:27 -0500

    Agregar archivos de proyecto.

---
+++ practica_sem7_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_sem7_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Años bisiestos desde 1900:");
+            for (int i = 1900; i <= 2023; i++)
+            {
+                if (i % 4 == 0 && i % 100 != 0) 
+                {
+                    Console.WriteLine(i);
+                }
+                if (i % 400 == 0) 
+                {
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
